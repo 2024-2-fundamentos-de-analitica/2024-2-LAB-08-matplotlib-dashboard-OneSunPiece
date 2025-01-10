c403402cd028b24b812c17b3fcc3c2956234e2cb
codeinpieces <68757620+codeinpieces@users.noreply.github.com>
2025-01-09 21:46:15 -0500

    D-0-N-3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,4 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
 # pylint: disable=line-too-long
+
 """
 Escriba el codigo que ejecute la accion solicitada.
 """
@@ -35,3 +40,97 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+    os.makedirs("docs", exist_ok=True)
+    df = pd.read_csv("files/input/shipping-data.csv")
+
+    # Grafica warehouse
+    data = df.copy()
+    plt.figure()
+    counts = data["Warehouse_block"].value_counts()
+    counts.plot.bar(
+        title="Shipping per warehouse",
+        xlabel="Warehouse block",
+        ylabel="Record count",
+        color="tab:blue",
+        fontsize=8
+    )
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    plt.savefig("docs/weight_distribution.png")
+
+    # Create index.html
+    with open("docs/index.html", "w") as f:
+        f.write("""
+        <html>
+        <head>
+            <title>Shipping Dashboard</title>
+        </head>
+        <body>
+            <h1>Shipping Dashboard</h1>
+            <h2>Shipping per warehouse</h2>
+            <img src="shipping_per_warehouse.png" alt="Shipping per warehouse">
+            <h2>Mode of shipment</h2>
+            <img src="mode_of_shipment.png" alt="Mode of shipment">
+            <h2>Average customer rating</h2>
+            <img src="average_customer_rating.png" alt="Average customer rating">
+            <h2>Shipping weight distribution</h2>
+            <img src="weight_distribution.png" alt="Shipping weight distribution">
+        </body>
+        </html>
+        """)
+    plt.savefig("docs/shipping_per_warehouse.png")
+    
+    # Grafica mode of shipment
+    plt.figure()
+    counts = data["Mode_of_Shipment"].value_counts()
+    counts.plot.pie(
+        title="Mode of shipment",
+        wedgeprops=dict(width=0.35),
+        ylabel="",
+        colors=["tab:blue", "tab:orange", "tab:green"],
+    )
+    plt.savefig("docs/mode_of_shipment.png")
+
+    # Grafica customer rating
+    plt.figure()
+    data = (data[["Mode_of_Shipment", "Customer_rating"]]
+            .groupby("Mode_of_Shipment").describe())
+    data.columns = data.columns.droplevel()
+    data = data[["mean","min","max"]]
+    plt.barh(
+        y=data.index.values,
+        width=data["max"].values -1,
+        left=data["min"].values,
+        height=0.9,
+        color="lightgray",
+        alpha=0.5
+    )
+    colors=["tab:green" if value>=3.0 else "tab:orange" for value in data["mean"].values]
+    plt.barh(
+        y=data.index.values,
+        width=data["max"].values - 1,
+        left=data["min"].values,
+        height=0.5,
+        color=colors,
+        alpha=1.0
+    )
+    plt.title("Average customer rating")
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().spines["left"].set_visible(False)
+    plt.gca().spines["bottom"].set_visible(False)
+
+    plt.savefig("docs/average_customer_rating.png")
+
+    # Grafica weight distribution
+    data=df.copy()
+    plt.figure()
+    data.Weight_in_gms.plot.hist(
+        title="Shipping weight distribution",
+        color="tab:orange",
+        edgecolor="white",
+    )
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.savefig("docs/weight_distribution.png")
